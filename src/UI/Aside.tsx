717a85b68d5189a14ec353f9c870295027554a1c
moessam919 <moessam919@gmail.com>
2024-10-31 13:22:26 +0300

    edit Aside Link

--- src/UI/Aside.tsx
+++ src/UI/Aside.tsx
@@ -14,29 +14,29 @@ interface AsideProps {
 
 const Aside = ({ isVisible, onNavClick, isMobile }: AsideProps) => {
     const linkClasses = ({ isActive }: { isActive: boolean }) => `
-        flex gap-2 justify-between items-center mb-5 
+        flex gap-2  items-center mb-5 
         hover:bg-gray-100 p-4 rounded-md w-full
-        ${isActive ? "bg-gray-200 font-bold" : ""}
+        ${isActive ? "bg-white font-bold" : ""}
     `;
 
     return (
         <aside
             className={`
-                bg-white shadow-lg flex flex-col items-center w-44 md:w-64
+                bg-[#e3e7ed] shadow-lg flex flex-col items-center w-44 md:w-64
                 transition-all duration-300 ease-in-out
                 ${isMobile ? "fixed right-0" : "fixed"}
                 ${isVisible ? "translate-x-0" : "translate-x-full"}
                 top-0 bottom-0 z-50
             `}
         >
-            <div className="mt-4 border-b-2 w-full flex flex-col items-center">
+            <div className="mt-4 border-b-2 border-gray-300 w-full flex flex-col items-center">
                 <div className="my-4 text-xl md:text-4xl font-bold">CRM</div>
             </div>
 
             <nav className="p-2 w-full">
                 <NavLink to="/" onClick={onNavClick} className={linkClasses}>
-                    <span>لوحة التحكم</span>
                     <LayoutDashboard />
+                    <span>لوحة التحكم</span>
                 </NavLink>
 
                 <NavLink
@@ -44,8 +44,8 @@ const Aside = ({ isVisible, onNavClick, isMobile }: AsideProps) => {
                     onClick={onNavClick}
                     className={linkClasses}
                 >
-                    <span>Lead</span>
                     <Handshake />
+                    <span>Lead</span>
                 </NavLink>
 
                 <NavLink
@@ -53,8 +53,8 @@ const Aside = ({ isVisible, onNavClick, isMobile }: AsideProps) => {
                     onClick={onNavClick}
                     className={linkClasses}
                 >
-                    <span>إدارة الطلبات</span>
                     <ChartPie />
+                    <span>إدارة الطلبات</span>
                 </NavLink>
 
                 <NavLink
@@ -62,8 +62,8 @@ const Aside = ({ isVisible, onNavClick, isMobile }: AsideProps) => {
                     onClick={onNavClick}
                     className={linkClasses}
                 >
-                    <span>الاتصال</span>
                     <SquareUserRound />
+                    <span>الاتصال</span>
                 </NavLink>
             </nav>
         </aside>
